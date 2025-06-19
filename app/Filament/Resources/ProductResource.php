7aa724f3f42e002bfa4cb40ff958cf3d4df01f78
Osama Ali <osamaali31121999@gmail.com>
2025-06-19 23:27:03 +0300

    Adding Product Variation Types and Options with Image Upload

--- app/Filament/Resources/ProductResource.php
+++ app/Filament/Resources/ProductResource.php
@@ -7,6 +7,7 @@ use App\Enums\RolesEnum;
 use App\Filament\Resources\ProductResource\Pages;
 use App\Filament\Resources\ProductResource\Pages\EditProduct;
 use App\Filament\Resources\ProductResource\Pages\ProductImages;
+use App\Filament\Resources\ProductResource\Pages\ProductVariationTypes;
 use App\Models\Product;
 use Filament\Facades\Filament;
 use Filament\Forms\Components\Grid;
@@ -147,6 +148,7 @@ class ProductResource extends Resource
             'create' => Pages\CreateProduct::route('/create'),
             'edit' => Pages\EditProduct::route('/{record}/edit'),
             'images' => Pages\ProductImages::route('/{record}/images'),
+            'variation-types' => Pages\ProductVariationTypes::route('/{record}/variation-types'),
         ];
     }
 
@@ -155,7 +157,8 @@ class ProductResource extends Resource
         return
             $page->generateNavigationItems([
                 EditProduct::class,
-                ProductImages::class
+                ProductImages::class,
+                ProductVariationTypes::class,
             ]);
     }
 
